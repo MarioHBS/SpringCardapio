cc7b6acbeb9dab2c6f6b4965fa971a35fbacfd4a
Mário Henrique <marioh90@gmail.com>
2023-10-14 21:46:33 -0300

    refactor: Modificada utilização do RestTemplate ao chamar dados da página #1

--- src/main/kotlin/com/mario/spring/cardapio_demo/services/HttpService.kt
+++ src/main/kotlin/com/mario/spring/cardapio_demo/services/HttpService.kt
@@ -1,26 +1,29 @@
 package com.mario.spring.cardapio_demo.services
 
-import com.mario.spring.cardapio_demo.domains.food.Food
 import com.mario.spring.cardapio_demo.utils.typeRef
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpMethod
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.HttpStatus
-import org.springframework.http.RequestEntity
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
-import java.net.URI
+import org.springframework.web.client.getForEntity
 
 @Service
-class HttpService(@Autowired private val restTemplate: RestTemplate) {
+class HttpService(@Autowired @Qualifier("restTemplateCustom") private val restTemplate: RestTemplate) {
 
     val url = "http://localhost:8080/food"
 
     fun carregarDados(): List<Map<String, *>> {
-        val request = RequestEntity<List<Map<String, *>>>(HttpMethod.GET, URI.create(url))
-        val response = restTemplate.exchange(request, typeRef<List<Map<String, *>>>())
+//        val request = RequestEntity<List<Map<String, *>>>(HttpMethod.GET, URI.create(url))
+//        val response = restTemplate.exchange(request, typeRef<List<Map<String, *>>>())
 
-        if (response.statusCode == HttpStatus.OK) {
-            response.body?.let { return it } ?: throw Exception("Corpo da requisição vazio")
+        val entity = restTemplate.getForEntity<List<Map<String, *>>>(
+            url,
+            typeRef<List<Map<String, *>>>()
+        )
+
+        if (entity.statusCode == HttpStatus.OK) {
+            entity.body?.let { return it } ?: throw Exception("Corpo da requisição vazio")
         }
 
         throw Exception("Nenhuma comida encontrada")
